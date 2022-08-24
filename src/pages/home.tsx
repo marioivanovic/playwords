6566f17fcd17dfbed8b042cd2089a3012846a958
Fadima-doumbia <doumbia.fari@gmail.com>
2022-08-24 21:19:16 +0200

    clavier

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,19 +8,27 @@ import {
   IonTitle,
   IonButton,
   IonToolbar,
+  IonFabButton,
 } from "@ionic/react";
 import "./home.css";
 import Keyboard from "./../components/keyboard";
 import data from "../Data.json";
 import { useEffect, useState } from "react";
+import '../components/keyboard.css'
+import clavier from "./../keyboards.json";
+
 
 const Home: React.FC = () => {
-
+  const arrayObject = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["B", "M", "N", "V", "C", "Z", "X"],
+  ];
   const rows = [];
 
   for (let row = 0; row < 6; row++) {
     let cols = [];
-    for (let col = 0; col < 7; col++) {
+    for (let col = 0; col < 5; col++) {
       cols.push({ key: "", value: "" });
     }
     rows.push(cols);
@@ -69,8 +77,8 @@ const Home: React.FC = () => {
                 <IonRow key={index}>
                   {row.length > 0
                     ? row.map((col, i) => (
-                        <IonCol key={i}>
-                          <IonButton onClick={handleClick} >A</IonButton>
+                        <IonCol key={i} size="2">
+                          <IonButton onClick={handleClick} >a</IonButton>
                         </IonCol>
                       ))
                     : null}
@@ -81,11 +89,17 @@ const Home: React.FC = () => {
         <h1>Le mot a deviner est : {random}</h1>
         <h1>Le mot a presser est : {press}</h1>
 
-
-        {/* <Keyboard arrayProps={clavier.top} />
-        <Keyboard arrayProps={clavier.middle} />
-        <Keyboard arrayProps={clavier.bottom} /> */}
-
+        <div className="keyboard-container">
+        {arrayObject.map((a, index)=>(
+          <IonRow key={index} className="keyboard-container-flex">
+              {a.map((keyValue, i)=>(
+                <IonCol size="1" className="keyboard-col" key={i}>
+                  <IonButton onClick={handleClick} size="small" className="keyboard-button">{keyValue}</IonButton>
+                </IonCol>
+              ))}
+          </IonRow>
+        ))}
+      </div>
       </IonContent>
     </IonPage>
   );
