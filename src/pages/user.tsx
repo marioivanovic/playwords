5e3ec2530ec65175b0b71602d04a9e99a41e7113
mrmarioi <ivanovic.mario@gmail.com>
2022-10-31 11:26:19 +0100

    :fire: final version push :fire:

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -13,7 +13,7 @@ const User: React.FC = () => {
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Play With Words </IonTitle>
+            <IonTitle size="large">Play With Words !</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonTitle size="small">Bienvenue {GetStat().pseudo} </IonTitle>
